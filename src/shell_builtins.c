53839552258e4aa9a39d38d8e41eae9b5eda9e90
hibazhd <hibazhd12@gmail.com>
2024-02-13 16:43:06 +0100

    made the code prettier and added first versio, of external execute

--- src/shell_builtins.c
+++ src/shell_builtins.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "shell_builtins.h"
+
+
+int is_builtin(struct cmdline* l){
+	return  (!strcmp(l->seq[0][0], "echo") || 
+		 	 !strcmp(l->seq[0][0], "pwd") ||
+		 	 !strcmp(l->seq[0][0], "whoami")
+		 );
+}
 
 void builtin_handler(){}
 
--- src/shell_builtins.h
+++ src/shell_builtins.h
@@ -1,5 +1,6 @@
 #ifndef _SHELL_BUILTINS_H
 #define _SHELL_BUILTINS_H
+#include "readcmd.h"
     enum BUILTINS{
         B_ECHO,
         B_PWD, 
@@ -7,4 +8,5 @@
     };
 
     void builtin_handler();
+    int is_builtin(struct cmdline *l);
 #endif
